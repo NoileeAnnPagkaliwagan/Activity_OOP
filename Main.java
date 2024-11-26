fc8649c12f164f453ae121df6f7cf7f6bb898114
NoileeAnnPagkaliwagan <23-16507@g.batstate-u.edu.ph>
2024-11-26 13:46:31 +0800

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args){
+
+        Dog puppy = new Dog();
+
+        puppy.setName("Aiah");
+
+        System.out.println(puppy.showName());
+
+        puppy.bark();
+
+        puppy.listofpuppies = new String[] {"Maloi", "Colet", "Gwen", "Mikha", "Staku", "Jho", "Sheena"};
+
+        int shownumber = puppy.show_numofpuppies();
+
+        System.out.println("Number of puppies: " + shownumber);
+
+        puppy.show_numofpuppies();
+
+        puppy.showpuppies();
+    }
+}
